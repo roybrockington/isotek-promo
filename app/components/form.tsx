f96e6f2bf2765ba2e7c284d9b886e854917a26dc
roybrockington <r.brockington@gmail.com>
2024-09-24 15:09:55 +0100

    React spinner on submission button

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -5,6 +5,7 @@ import Intro from './intro'
 import { useState } from "react"
 import Terms from "./terms"
 import { useRouter } from "next/navigation"
+import { BarLoader, BounceLoader, ClipLoader, ClockLoader, FadeLoader, PulseLoader, ScaleLoader } from "react-spinners"
 
 const variants = {
     open: { opacity: 1, x: 0, transition: { staggerChildren: 0.07, delayChildren: 0.2 }},
@@ -14,6 +15,7 @@ const variants = {
 
 const Form = () => {
     const [isOpen, setIsOpen] = useCycle(false, true)
+    const [loading, setLoading] = useState(false)
     const [data, setData] = useState({
         name: '',
         email: '',
@@ -36,6 +38,7 @@ const Form = () => {
 
     const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        setLoading(true)
         try {
             const response = await fetch('/api/register', {
                 method: 'post',
@@ -147,7 +150,16 @@ const Form = () => {
                         </label>
                     </div>
                     <div className="flex justify-center mb-5">
-                        <button type="submit" className="text-white bg-isotek hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <button type="submit" className="flex justify-center items-center gap-2 text-white bg-isotek hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Submit <ScaleLoader
+        color={'white'}
+        loading={loading}
+        height={10}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+
+                        </button>
                     </div>
                 </motion.form>
             }
